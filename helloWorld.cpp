b548d258e0fc72855b3768c48f00f5c036c41fec
Michael Clermont <michael.clermont@mths.ca>
2022-02-17 19:45:29 +0000

    added hello world

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright(c) 2022 Michael Clermont All Rights Reserved
+//
+// Created by Michael Clermont
+// Created on February 2022
+// This is a "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
